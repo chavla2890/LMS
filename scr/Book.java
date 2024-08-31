15635d2babfa6fb66e4cfe394626064d585947c4
AAKASH PRAKASHBHAI CHAVLA <akashchawla497@gmail.com>
2024-08-31 18:12:22 +0530

    Creating Book class

---
+++ scr/Book.java
@@ -0,0 +1,53 @@
+public class Book {
+    private String isbn;
+    private String title;
+    private String author;
+    private int year;
+    private boolean available = true;
+
+    public Book(String isbn, String title, String author, int year) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return isbn.equals(book.isbn);
+    }
+
+    @Override
+    public int hashCode() {
+        return isbn.hashCode();
+    }
+}
